0217ff159a133d2eae49fc946455aaccda2b96cf
Ian Bell <ian.h.bell@gmail.com>
2013-09-18 23:09:17 +0200

    Tests run
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

--- wrappers/Python/test.py
+++ wrappers/Python/test.py
@@ -9,7 +9,6 @@ print InternalFlow.Lockhart_Martinelli_1949_DPDZ_f(CPS,100,0.1,0.5)
 
 sys.path.append('../../../achp_git/src')
 import ACHP
-
 CPS2 = ACHP.CoolPropStateClassSI("Propane")
 CPS2.update(ACHP.iT,300,ACHP.iQ,0.5)
 print InternalFlow.Lockhart_Martinelli_1949_DPDZ_f(CPS2,100,0.1,0.5)
